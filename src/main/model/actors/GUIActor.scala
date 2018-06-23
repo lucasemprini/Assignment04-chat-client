100a28b7ffc56e140e509c37344d2de42df39230
Luca Semprini <luca.semprini10@studio.unibo.it>
2018-06-23 17:08:14 +0200

    Aggiustato errore a runtime in GUIActor

--- src/main/model/actors/GUIActor.scala
+++ src/main/model/actors/GUIActor.scala
@@ -2,15 +2,15 @@ package model.actors
 
 import java.awt.Label
 
-import akka.actor.{AbstractActor, ActorRef}
+import akka.actor.{AbstractActor, Actor, ActorRef}
 import javafx.collections.ObservableList
 import model.messages._
 
 class GUIActor(users: ObservableList[ActorRef], currentChat: ObservableList[String],
                registry: ActorRef, mapOfChats: Map[ActorRef, ObservableList[String]],
-               actorLabel: Label) extends AbstractActor {
+               actorLabel: Label) extends Actor {
 
-  override def createReceive(): Receive = {
+  override def receive(): Receive = {
       case SendButtonMsg(message, listOfMessages, sender) => //TODO invio del messaggio. Come gestirlo?
       case NewChatButtonMsg(chatName) => //TODO nuova chat creata. Come gestirlo?
       case RemoveChatButtonMsg(removeWho)=> //TODO rimozione chat. Come gestirlo?
